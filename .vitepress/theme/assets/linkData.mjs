5809d767944ae5e9bb83c9704d662159c9c549ba
V01d <ba20240121@gmail.com>
2026-04-14 17:25:02 +0800

    Modify friend list and update entry details
    Updated a friend's entry and removed multiple entries.

---
+++ .vitepress/theme/assets/linkData.mjs
@@ -0,0 +1,41 @@
+// 链接数据
+const linkData = [
+  // 推荐数据
+  {
+    type: "rec",
+    typeName: "本站",
+    typeDesc: "自己的东西",
+    typeList: [
+      {
+        name: "void data sea",
+        avatar: "/images/logo/logo.webp",
+        desc: "！！！",
+        url: "https://blog.v01d.fyi",
+      },
+    ],
+  },
+  // 小伙伴们
+  {
+    type: "friends",
+    typeName: "小伙伴们",
+    typeDesc: "我们在一起，共同进步",
+    typeList: [
+      //{
+      //  name: "",
+      // avatar: "",
+      //  desc: "",
+      //  url: "",
+      //},
+    ],
+  },
+    // 丢失
+//  {
+//    type: "lost",
+//    typeName: "走丢了",
+//    typeDesc: "因为各种原因失联的站点",
+//    typeList: [
+//    ],
+//  },
+];
+
+export default linkData;
